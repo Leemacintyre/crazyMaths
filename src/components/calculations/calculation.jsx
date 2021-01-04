4c7bb34c4fdc7c0c4d93d30942f32232a6c804c4
Lee Macintyre <macintyrelee@gmail.com>
2021-01-04 14:08:32 +0000

    stored the user input value in the redux store

--- src/components/calculations/calculation.jsx
+++ src/components/calculations/calculation.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './calculation.style.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
-import { randomNum1, isCorrect, isIncorrect, counter } from '../../actions';
+import {
+	randomNum1,
+	isCorrect,
+	isIncorrect,
+	counter,
+	inputValue,
+} from '../../actions';
 import DifficultyButtons from '../difficultyButtons/difficultyButtons';
 
 export const Calculation = () => {
@@ -11,27 +17,27 @@ export const Calculation = () => {
 	const difficultyLevel = useSelector((state) => state.difficulty);
 	const answerState = useSelector((state) => state.isCorrect);
 
-	const [inputValue, setInputValue] = useState('');
-	//const [subtraction, setSubtraction] = useState('-');
-	//const noSubtraction = setSubtraction(parseInt(subtraction));
-	//console.log(subtraction);
+	const stateInputValue = useSelector((state) => state.inputValue);
 
-	const checkAnswer = (event) => {
+	console.log(stateInputValue);
+
+	const checkAnswer = () => {
 		const total = num1 + num2;
 		const strTotal = total.toString();
-		setInputValue(event.target.value);
-		console.log(answerState);
-		console.log(event.target.value);
 
-		if (event.target.value === strTotal) {
-			setInputValue('');
+		if (stateInputValue === strTotal) {
 			dispatch(randomNum1(difficultyLevel));
 			dispatch(isCorrect());
 			dispatch(counter());
-		} else if (event.target.value.length > 0) {
+			dispatch(inputValue(''));
+		} else if (stateInputValue.length > 0) {
 			dispatch(isIncorrect());
 		}
 	};
+
+	useEffect(() => {
+		checkAnswer();
+	});
 
 	return (
 		<div className={styles.container}>
@@ -45,7 +51,13 @@ export const Calculation = () => {
 
 			<div className={styles.inputContainer}>
 				<p>{answerState ? 'Correct' : 'Incorrect'}</p>
-				<input value={inputValue} onChange={checkAnswer} />
+				<input
+					value={stateInputValue}
+					onChange={(event) => {
+						checkAnswer();
+						dispatch(inputValue(event.target.value));
+					}}
+				/>
 			</div>
 		</div>
 	);
--- src/reducers/inputValue.js
+++ src/reducers/inputValue.js
@@ -1,7 +1,7 @@
 export const inputValue = (state = '', action) => {
 	switch (action.type) {
 		case 'INPUT_VALUE':
-			return action.payload;
+			return (state = action.payload);
 
 		default:
 			return state;
